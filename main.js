c58a68cf6059215ca622a0a91a5234cd5263e2cb
smile22-9 <79411348+smile22-9@users.noreply.github.com>
2023-07-03 05:45:41 +0700

    new update

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import bg1 from '../threejs/bg1.jpg'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, 
 	window.innerWidth / window.innerHeight, 
@@ -18,40 +19,52 @@ renderer.shadowMap.enabled = true;
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+function createBox() {
+	const cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
+	const cubeMaterial = new THREE.MeshBasicMaterial( { color: 0x73a9ad } );
+	const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+	cube.castShadow = true;
+	scene.add(cube);
+}	
 
-const cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
-const cubeMaterial = new THREE.MeshBasicMaterial( { color: 0x73a9ad } );
-
-const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
-cube.castShadow = true;
-scene.add(cube);
-
-
-// mặt phẳng
-function addPlane(planeGeometry, planeMaterial){
-	const plane = new THREE.Mesh(planeGeometry,planeMaterial);
-	plane.rotation.x = -0.5 * Math.PI;
-	plane.receiveShadow = true;
-	scene.add(plane);
-};
+const cBox = document.getElementById('createBox');
+cBox.addEventListener('click',createBox);
 
 
-const planeGeometry = new THREE.PlaneGeometry(30,30);
-const planeMaterial = new THREE.MeshStandardMaterial({color: 0xa281d8});
+// createBox();
+// const cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const cubeMaterial = new THREE.MeshBasicMaterial( { color: 0x73a9ad } );
 
-addPlane(planeGeometry, planeMaterial);
+// const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+// cube.castShadow = true;
+// scene.add(cube);
+
+
+// // mặt phẳng
+// function addPlane(planeGeometry, planeMaterial){
+// 	const plane = new THREE.Mesh(planeGeometry,planeMaterial);
+// 	plane.rotation.x = -0.5 * Math.PI;
+// 	plane.receiveShadow = true;
+// 	scene.add(plane);
+// };
+
+
+// const planeGeometry = new THREE.PlaneGeometry(30,30);
+// const planeMaterial = new THREE.MeshStandardMaterial({color: 0xa281d8});
+
+// addPlane(planeGeometry, planeMaterial);
 
 
 
-//hình cầu
-const sphereGeomatry = new THREE.SphereGeometry(4);
-const sphereMeterial = new THREE.MeshBasicMaterial({
-	color: 0xe2c0c0,
-	wireframe: true
-});
-const sphere = new THREE.Mesh(sphereGeomatry,sphereMeterial);
+// //hình cầu
+// const sphereGeomatry = new THREE.SphereGeometry(4);
+// const sphereMeterial = new THREE.MeshBasicMaterial({
+// 	color: 0xe2c0c0,
+// 	wireframe: true
+// });
+// const sphere = new THREE.Mesh(sphereGeomatry,sphereMeterial);
 
-scene.add(sphere);
+// scene.add(sphere);
 
 
 
@@ -66,37 +79,37 @@ scene.add( axesHelper );
 camera.position.z = 10;
 orbit.update();
 
-const textureLoader = new THREE.TextureLoader();
-scene.background = textureLoader.load(bg1);
+// const textureLoader = new THREE.TextureLoader();
+// scene.background = textureLoader.load(bg1);
 
-const gui = new dat.GUI();
-const option = {
-	cubeColor:'#73A9AD',
-	sphereColor:'#e2c0c0',
-	planeColor:'#a281d8',
-};
+// const gui = new dat.GUI();
+// const option = {
+// 	cubeColor:'#73A9AD',
+// 	sphereColor:'#e2c0c0',
+// 	planeColor:'#a281d8',
+// };
 
-gui.addColor(option,'cubeColor').onChange(function(e){
-	cube.material.color.set(e)
-})
+// gui.addColor(option,'cubeColor').onChange(function(e){
+// 	cube.material.color.set(e)
+// })
 
-gui.addColor(option,'sphereColor').onChange(function(e){
-	sphere.material.color.set(e)
-})
+// gui.addColor(option,'sphereColor').onChange(function(e){
+// 	sphere.material.color.set(e)
+// })
 
-gui.addColor(option,'planeColor').onChange(function(e){
-	plane.material.color.set(e)
-})
+// gui.addColor(option,'planeColor').onChange(function(e){
+// 	plane.material.color.set(e)
+// })
 
-gui.add(cube.rotation,'x', 0 , Math.PI).name('rotate Cube\'s X Axis');
-gui.add(cube.rotation,'y', 0 , Math.PI).name('rotate Cube\'s Y Axis');
-gui.add(cube.rotation,'z', 0 , Math.PI).name('rotate Cube\'s Z Axis');
-gui.add(cube.position,'x', 0 , 4).name('Position Cube\'s X Axis');
-gui.add(cube.position,'y', 0 , 4).name('Position Cube\'s Y Axis');
-gui.add(cube.position,'z', 0 , 4).name('position Cube\'s Z Axis');
-gui.add(cube.scale,'x', 0 , 5).name('scale Cube\'s X Axis');
-gui.add(cube.scale,'y', 0 , 5).name('scale Cube\'s Y Axis');
-gui.add(cube.scale,'z', 0 , 5).name('scale Cube\'s Z Axis');
+// gui.add(cube.rotation,'x', 0 , Math.PI).name('rotate Cube\'s X Axis');
+// gui.add(cube.rotation,'y', 0 , Math.PI).name('rotate Cube\'s Y Axis');
+// gui.add(cube.rotation,'z', 0 , Math.PI).name('rotate Cube\'s Z Axis');
+// gui.add(cube.position,'x', 0 , 4).name('Position Cube\'s X Axis');
+// gui.add(cube.position,'y', 0 , 4).name('Position Cube\'s Y Axis');
+// gui.add(cube.position,'z', 0 , 4).name('position Cube\'s Z Axis');
+// gui.add(cube.scale,'x', 0 , 5).name('scale Cube\'s X Axis');
+// gui.add(cube.scale,'y', 0 , 5).name('scale Cube\'s Y Axis');
+// gui.add(cube.scale,'z', 0 , 5).name('scale Cube\'s Z Axis');
 
 //light
 const ambientLight = new THREE.AmbientLight(0x333333);
@@ -107,45 +120,45 @@ directionalLight.position.set(-30,50,0);
 directionalLight.castShadow = true;
 scene.add(directionalLight);
 
-// const drLightHelper = new THREE.DirectionalLightHelper(directionalLight);
-// scene.add(drLightHelper);
+const drLightHelper = new THREE.DirectionalLightHelper(directionalLight);
+scene.add(drLightHelper);
 
-function loadmodel(model){
-		// Instantiate a loader
-	const loader = new GLTFLoader();
+// function loadmodel(model){
+// 		// Instantiate a loader
+// 	const loader = new GLTFLoader();
 
-	// Load a glTF resource
-	loader.load(
-		// resource URL
-		model,
-		// called when the resource is loaded
-		function ( gltf ) {
+// 	// Load a glTF resource
+// 	loader.load(
+// 		// resource URL
+// 		model,
+// 		// called when the resource is loaded
+// 		function ( gltf ) {
 
-			scene.add( gltf.scene );
+// 			scene.add( gltf.scene );
 
-			gltf.animations; // Array<THREE.AnimationClip>
-			gltf.scene; // THREE.Group
-			gltf.scenes; // Array<THREE.Group>
-			gltf.cameras; // Array<THREE.Camera>
-			gltf.asset; // Object
+// 			gltf.animations; // Array<THREE.AnimationClip>
+// 			gltf.scene; // THREE.Group
+// 			gltf.scenes; // Array<THREE.Group>
+// 			gltf.cameras; // Array<THREE.Camera>
+// 			gltf.asset; // Object
 
-		},
-		// called while loading is progressing
-		function ( xhr ) {
+// 		},
+// 		// called while loading is progressing
+// 		function ( xhr ) {
 
-			console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+// 			console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 
-		},
-		// called when loading has errors
-		function ( error ) {
+// 		},
+// 		// called when loading has errors
+// 		function ( error ) {
 
-			console.log( 'An error happened' );
+// 			console.log( 'An error happened' );
 
-		}
-	);
-}
+// 		}
+// 	);
+// }
 
-loadmodel('model/dining_table/scene.gltf')
+// loadmodel('model/dining_table/scene.gltf')
 
 function animate() {
 	requestAnimationFrame( animate );
